d8e5495b8dbf0712a2ef01982854689d9b014a83
Anderson Vieira <asvieira.br@gmail.com>
2022-10-31 22:30:19 -0300

    fix: adjusting spacing between sections

--- src/App.tsx
+++ src/App.tsx
@@ -34,8 +34,8 @@ export default function App() {
           </ButtonCardCreate>
         </div>
       </main>
-      <section className='mx-6 px-3 py-6 rounded bg-stone-800 border-2 border-cyan-500'>
-        <header className='text-center'>
+      <section className='mx-6 px-3 py-8 rounded bg-stone-800 border-2 border-cyan-500'>
+        <header className='text-center mb-12'>
           <Text className='text-lg font-semibold'>Preencha os dados e crie o seu próprio cartão!</Text>
         </header>
           <form className='text-center'>
@@ -65,9 +65,11 @@ export default function App() {
               <Text>Suas habilidades</Text>
             </label>
             <Input type='text' name='skills' id='skills' placeholder='Selecione até 4 habilidades'></Input>
-            <ButtonCardCreate>
-            <Text className='font-semibold text-md'>Gerar cartão</Text>
-            </ButtonCardCreate>
+            <div className='mt-12'>
+              <ButtonCardCreate>
+              <Text className='font-semibold text-md'>Gerar cartão</Text>
+              </ButtonCardCreate>
+            </div>
           </form>
       </section>
     </div>
